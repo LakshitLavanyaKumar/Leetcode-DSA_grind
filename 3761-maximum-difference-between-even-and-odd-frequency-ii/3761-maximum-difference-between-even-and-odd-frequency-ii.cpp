2066af0780fddcc897dd8c855de4a5777d33b4a6
LakshitLavanyaKumar <153279175+LakshitLavanyaKumar@users.noreply.github.com>
2025-06-12 08:47:27 +0530

    Time: 50 ms (78.38%), Space: 12.4 MB (72.07%) - LeetHub

--- 3761-maximum-difference-between-even-and-odd-frequency-ii/3761-maximum-difference-between-even-and-odd-frequency-ii.cpp
+++ 3761-maximum-difference-between-even-and-odd-frequency-ii/3761-maximum-difference-between-even-and-odd-frequency-ii.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
-int getStatus(int a , int b)
+int getStatus(int a ,int b)
 {
-    return ((a & 1) << 1) | (b & 1);
+    return ((a&1)<<1) | (b&1);
 }
     int maxDifference(string s, int k) {
-        int n  = s.length();
-        int ans =INT_MIN;
-        for(char a= '0';a<='4';a++)
+  //sochna yehi tha ki har pair k liye check krnqa hai
+  //sliding window hi hai par har pair k l9ye hai ....my constraints are not that big
+  int n = s.length();
+  int ans = INT_MIN;
+   for(char a='0';a<='4';a++)
+   {
+    for(char b ='0';b<='4';b++)
+    {
+      int ca = 0;
+      int cb =0;
+      int pa =0;int pb =0;
+      int left =-1;
+      vector<int> best(4,INT_MAX);
+      for(int right =0;right<n;right++)
+      {
+        ca+=(s[right] == a);
+        cb+=(s[right] == b);
+        while(right -left>=k && cb-pb>=2)
         {
-            for(char b ='0';b<='4';b++)
-            {
-                int ca =0;
-                int cb =0;
-                vector<int> best(4,INT_MAX);
-                int pa =0;int pb =0;
-                int right = 0;
-                int left =-1;
-                for(int right =0;right<n;right++)
-                {
-                    ca+=(s[right] == a);
-                    cb+=(s[right] == b);
-                   while(right -left>=k && cb-pb>=2)
-                   {
-                        int left_status = getStatus(pa, pb);
-                        best[left_status] = min(best[left_status], pa - pb);
-                        left++;
-                        pa+=(s[left]==a);
-                        pb+=(s[left]==b);
-                   }
-                   int right_status = getStatus(ca, cb);
-                    int required_status = right_status ^ 0b10;
-
-                    if (best[required_status] != INT_MAX) {
-                        ans = max(ans, ca - cb - best[required_status]);
-                    }
-
-                }
-            }
-        }
-        return ans;
+            int lefts = getStatus(pa,pb);
+            best[lefts] =min(best[lefts ], pa-pb);
+            left++;
+            pa+=(s[left] == a);
+            pb+=(s[left] == b);
+        } 
+      
+      int r = getStatus(ca,cb);
+      int req = r^ 0b10;
+  if(best[req]!=INT_MAX)
+  {
+    ans = max(ans, ca-cb-best[req]);
+  }
+    }
+   }
+   }
+   return ans;
     }
 };
