48a3d16874aa36634a709c8dbe5596fbd139298a
LakshitLavanyaKumar <153279175+LakshitLavanyaKumar@users.noreply.github.com>
2025-07-16 14:34:51 +0530

    Time: 0 ms (100%), Space: 10.2 MB (54.82%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,44 +1,76 @@
 class Solution {
 public:
-   void f(int node , vector<bool> &cols, int n,vector<vector<string>> &ans,vector<string> &p)
-    {
-        if(p.size()==n)
-        {ans.push_back(p);
-        return;}
-      //kisme ni jaa sakta unko nikaaldeta hun baad mai loop to lagana hi hai
-      int l = max(node-1, 0);
-      int r = min(node+1,n-1);
-      int c = node;
-      for(int i =0;i<n;i++)
-      {
-        if(i!=l && i!=c && i!=r && cols[i]==true)
-        {
-     string s(n,'.');
-     cols[i] =false;
-            s[i] = 'Q';
-           p.push_back(s);
-            f(i , cols , n , ans ,p);
-            p.pop_back();
-            cols[i] =true;
+bool issafe(int r,int c,vector<string> &board , int n)
+{
+   // bool f = true;
+for(int i =0;i<n;i++)
+{
+    if(board[i][c]=='Q' && i!=r)
+    {return false;}
+}
+for(int i =0;i<n;i++)
+{
+    if(board[r][i]=='Q' && i!=c)
+    {return false;}
+}
+int x =r-1;int y =c-1;
+while(x>=0 && y>=0)
+{
+    if(board[x][y]=='Q')
+    {return false;}
+    x--;y--;
+}
+int i1 = r + 1, j1 = c + 1;
+while (i1 < n && j1 < n) {
+    if (board[i1][j1] == 'Q') {
+        return false;
+    }
+    i1++; j1++;
+}
+
+int i2 = r - 1, j2 = c + 1;
+while (i2 >= 0 && j2 < n) {
+    if (board[i2][j2] == 'Q') {
+        return false;
+    }
+    i2--; j2++;
+}
+
+int i3 = r + 1, j3 = c - 1;
+while (i3 < n && j3 >= 0) {
+    if (board[i3][j3] == 'Q') {
+        return false;
+    }
+    i3++; j3--;
+}
+
+return true;
+}
+void put(int row  ,vector<string> &board , int n,vector<vector<string>>  &ans)
+{
+    if (row == n) {
+            ans.push_back(board);
+            return;
         }
-      }
-    } 
+
+        for (int col = 0; col < n; col++) {
+            if (issafe(row, col, board, n)) {
+                board[row][col] = 'Q';
+                put(row + 1, board, n, ans);
+                board[row][col] = '.';
+            }
+        }
+}
     vector<vector<string>> solveNQueens(int n) {
-        if(n==2 || n==3)
-        {return {};}
-        vector<bool> cols(n,true);
-        vector<vector<string>> ans;
-        vector<string> p;
-        for(int i =0;i<n;i++)
-        {
-            cols[i] =false;
-            string s(n,'.');
-            s[i] = 'Q';
-            p.push_back(s);
-            f(i , cols , n , ans,p);
-            
-            cols[i] =true;
-        }
-        return ans;
+        //string s(n,'.');
+  vector<string>  board;
+  vector<vector<string>>  ans;
+  string s(n,'.');
+  for(int i =0;i<n;i++)
+  {board.push_back(s);}
+  int k =0;
+  // i have put k queens as of now
+  put(0,board,n,ans);
+  return ans;
     }
 };
