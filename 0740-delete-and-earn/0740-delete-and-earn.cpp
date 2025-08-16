397798aad2324c7ac202e23fe9c8d82bef14887e
LakshitLavanyaKumar <153279175+LakshitLavanyaKumar@users.noreply.github.com>
2025-08-16 16:44:32 +0530

    Time: 78 ms (5.03%), Space: 18.8 MB (20.36%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        int n  = nums.size();
+     sort(nums.begin() , nums.end());
+     map<int,int> mp;
+     set<int>st;
+     for(int i =0;i<n;i++)
+     {
+        mp[nums[i]]+=nums[i];
+        st.insert(nums[i]);
+     }   
+     vector<int > f(st.begin() , st.end() );
+     int n1 = f.size();
+          for(auto it:f)
+     {cout<<it<<" ";}
+     cout<<endl;
+     vector<int> dp(n1+2,0);
+     for(int i =n1;i>0;i--)
+     {
+        //pick
+        int notpick = dp[i+1];
+        // pick
+        int pick = mp[f[i-1]];
+        int j =i+1;
+        while(j<=n1 && (f[j-1]==(f[i-1]+1)))
+        {j++;} 
+        pick += dp[j];
+        dp[i] =max(notpick ,pick); 
+
+     }
+     for(int i =0;i<=n1+1;i++)
+     {cout<<dp[i]<<" ";}
+     cout<<endl;
+     return dp[1];
+    }
+};
