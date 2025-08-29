be6b09e292a1c8df79c0164152df10bed9b1a8ae
LakshitLavanyaKumar <153279175+LakshitLavanyaKumar@users.noreply.github.com>
2025-08-29 19:57:18 +0530

    Time: 0 ms (100%), Space: 8.5 MB (49.32%) - LeetHub

---
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int x = 0;
+        x = (n*n);
+         int y = 0;
+        y = (n*n +n);
+        return n;
+        
+    }
+};
