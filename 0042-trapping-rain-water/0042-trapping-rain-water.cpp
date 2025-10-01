7f82dad0d2c523a70dfb36dd2c2987dec7c7e3e4
LakshitLavanyaKumar <153279175+LakshitLavanyaKumar@users.noreply.github.com>
2025-10-02 01:53:36 +0530

    Time: 4 ms (10.65%), Space: 27.2 MB (30.92%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,23 +2,27 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        stack<int> st;
-        int  waterTrapped =0;
-        for(int i=0;i<n;i++)
+        vector<int > pm(n) ,sm(n);
+        pm[0] =height[0];
+        sm[n-1] = height[n-1];
+        for(int i =1;i<n;i++)
         {
-            while(!st.empty() && height[i]>height[st.top()])
-            {
-                int t = st.top();
-                st.pop();
-                if(st.size() ==0)
-                {break;}
-                 int distance = i - st.top() - 1;
-            int bounded_height = min(height[i], height[st.top()]) - height[t];
-            waterTrapped += distance * bounded_height;
-            }
-        
-        st.push(i);
+            pm[i] = max(pm[i-1] , height[i]);
+
         }
-        return waterTrapped;
+               for(int i =n-2;i>=0;i--)
+        {
+            sm[i] = max(sm[i+1] , height[i]);
+
+        }
+        int ans =0;
+        for(int i =0;i<n;i++)
+        {
+            if(height[i]<=pm[i] && height[i]<=sm[i])
+            {
+                ans+= (min(pm[i] , sm[i]) - height[i]);
+            }
+        }
+        return ans;
     }
 };
