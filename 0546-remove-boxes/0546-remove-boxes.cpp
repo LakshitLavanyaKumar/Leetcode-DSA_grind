d61aee3c3da9f2c0a76a608b029f4c81d67d74e7
LakshitLavanyaKumar <153279175+LakshitLavanyaKumar@users.noreply.github.com>
2025-10-08 14:44:58 +0530

    Time: 693 ms (5.07%), Space: 14.1 MB (97.99%) - LeetHub

--- 0546-remove-boxes/0546-remove-boxes.cpp
+++ 0546-remove-boxes/0546-remove-boxes.cpp
@@ -2,33 +2,31 @@ class Solution {
 public:
     int removeBoxes(vector<int>& boxes) {
         int n = boxes.size();
-        int dp[101][101][101];
-        memset( dp , 0 , sizeof( dp ) ) ;
-        for(int  len =1;len<=n;len++)
+        static int dp[101][101][101];
+        memset(dp,0,sizeof(dp));
+        for(int len =1;len<=n;len++)
         {
             for(int i = n-len;i>=0;i--)
             {
                 int j = i+len-1;
-                for(int k=0;k<n;k++)
+                for(int k =0;k<=n;k++)
                 {
                     int cnt =1;
-                    while(i+cnt<=j && boxes[i+cnt]==boxes[i]){cnt++;}
+                    while(i+cnt<=j && boxes[i+cnt] == boxes[i])
+                    {cnt++;}
+                    int best = (cnt+k)*(cnt+k) + dp[i+cnt][j][0];
 
-                    int best = ((k+cnt)*(k+cnt)) + (i+cnt<=j?dp[i+cnt][j][0]:0);
-
-                    for(int m = i+cnt;m<=j;m++)
+                    for(int m =i+cnt;m<=j;m++)
                     {
                         if(boxes[m] == boxes[i])
                         {
-                            int val = dp[i+cnt][m-1][0]+(k+cnt<=n?dp[m][j][k+cnt]:0);
-                            best = max(best ,val);
+                            best = max(best , dp[i+cnt][m-1][0] + (k+cnt<=n?dp[m][j][k+cnt]:0));
                         }
                     }
-                    dp[i][j][k] = best;
+                    dp[i][j][k] =best;
                 }
-
             }
         }
-    return dp[0][n-1][0];
+        return dp[0][n-1][0];
     }
 };
