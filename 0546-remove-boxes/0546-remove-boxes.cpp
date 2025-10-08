118ab3db17789f3d5bb137d5290950fe1b52dcf4
LakshitLavanyaKumar <153279175+LakshitLavanyaKumar@users.noreply.github.com>
2025-10-08 14:56:11 +0530

    Time: 430 ms (7.51%), Space: 14.1 MB (97.99%) - LeetHub

--- 0546-remove-boxes/0546-remove-boxes.cpp
+++ 0546-remove-boxes/0546-remove-boxes.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int removeBoxes(vector<int>& boxes) {
-        int n = boxes.size();
+        int n =boxes.size();
         static int dp[101][101][101];
         memset(dp,0,sizeof(dp));
         for(int len =1;len<=n;len++)
         {
-            for(int i = n-len;i>=0;i--)
+            for(int i =n-len;i>=0;i--)
             {
-                int j = i+len-1;
+                int j =i+len-1;
                 for(int k =0;k<=n;k++)
                 {
                     int cnt =1;
-                    while(i+cnt<=j && boxes[i+cnt] == boxes[i])
-                    {cnt++;}
-                    int best = (cnt+k)*(cnt+k) + dp[i+cnt][j][0];
-
-                    for(int m =i+cnt;m<=j;m++)
+                    while(i+cnt<=j && boxes[i+cnt]==boxes[i]){cnt++;}
+                    int best = (k+cnt)*(k+cnt) + dp[i+cnt][j][0];
+                    for(int m = i+cnt;m<=j;m++)
                     {
-                        if(boxes[m] == boxes[i])
+                        if(boxes[i] ==boxes[m])
                         {
-                            best = max(best , dp[i+cnt][m-1][0] + (k+cnt<=n?dp[m][j][k+cnt]:0));
+                            best =max(best , dp[i+cnt][m-1][0] +(k+cnt<=n?dp[m][j][k+cnt]:0));
                         }
                     }
                     dp[i][j][k] =best;
