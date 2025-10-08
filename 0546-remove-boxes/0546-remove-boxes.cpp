c565e1a2134c9581bcee91ab06c2e1fab87e2dd4
LakshitLavanyaKumar <153279175+LakshitLavanyaKumar@users.noreply.github.com>
2025-10-08 06:27:46 +0530

    Time: 726 ms (5.07%), Space: 56.9 MB (40.61%) - LeetHub

---
+++ 0546-remove-boxes/0546-remove-boxes.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int removeBoxes(vector<int>& boxes) {
+        int n = boxes.size();
+        int dp[1021][102][102];
+        memset( dp , 0 , sizeof( dp ) ) ;
+        for(int  len =1;len<=n;len++)
+        {
+            for(int i = n-len;i>=0;i--)
+            {
+                int j = i+len-1;
+                for(int k=0;k<=n;k++)
+                {
+                    int cnt =1;
+                    while(i+cnt<=j && boxes[i+cnt]==boxes[i]){cnt++;}
+
+                    int best = ((k+cnt)*(k+cnt)) + (i+cnt<=j?dp[i+cnt][j][0]:0);
+
+                    for(int m = i+cnt;m<=j;m++)
+                    {
+                        if(boxes[m] == boxes[i])
+                        {
+                            int val = dp[i+cnt][m-1][0]+(k+cnt<=n?dp[m][j][k+cnt]:0);
+                            best = max(best ,val);
+                        }
+                    }
+                    dp[i][j][k] = best;
+                }
+
+            }
+        }
+    return dp[0][n-1][0];
+    }
+};
